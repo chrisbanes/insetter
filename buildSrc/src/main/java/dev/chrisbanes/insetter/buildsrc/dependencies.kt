96fd4e7da13a101d86c060023ee050444770ec97
Chris Banes <chris@banes.me>
2020-12-23 12:55:23 +0000

    Update to AndroidX Core 1.5.0-alpha05

--- buildSrc/src/main/java/dev/chrisbanes/insetter/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/insetter/buildsrc/dependencies.kt
@@ -51,8 +51,8 @@ object Libs {
             const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
         }
 
-        const val core = "androidx.core:core:1.5.0-alpha04"
-        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha04"
+        const val core = "androidx.core:core:1.5.0-alpha05"
+        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha05"
     }
 
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0"
