59c2944b7cbb42838b2cbe8941ed23aac5f7def5
Chris Banes <chris@banes.me>
2021-02-11 17:41:58 +0000

    Add some missing kdoc

--- library/src/main/java/dev/chrisbanes/insetter/ViewDimensions.kt
+++ library/src/main/java/dev/chrisbanes/insetter/ViewDimensions.kt
@@ -16,13 +16,21 @@
 
 package dev.chrisbanes.insetter
 
+import androidx.annotation.Px
+
+/**
+ * A data class which represents a pixel length on each dimension of a view.
+ */
 data class ViewDimensions(
-    val left: Int,
-    val top: Int,
-    val right: Int,
-    val bottom: Int
+    @Px val left: Int,
+    @Px val top: Int,
+    @Px val right: Int,
+    @Px val bottom: Int
 ) {
     companion object {
+        /**
+         * An instance of [ViewDimensions] which is empty.
+         */
         @JvmField
         val EMPTY = ViewDimensions(0, 0, 0, 0)
     }
