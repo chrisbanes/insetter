b45ab42b7c271f3b07b2f74294f5b021eace55a0
Chris Banes <chris@banes.me>
2021-05-25 09:28:38 +0100

    Update generate_docs.sh

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fail on any error
+set -ex
 
 DOCS_ROOT=docs-gen
 
@@ -8,15 +25,7 @@ mkdir $DOCS_ROOT
 # Clear out the old API docs
 [ -d docs/api ] && rm -r docs/api
 # Build the docs with dokka
-./gradlew clean dokkaHtmlMultiModule
-
-# Dokka doesn't currently allow us to change the index page name so move it manually
-mv docs/api/-modules.html docs/api/index.html
-
-# Re-word the Dokka call out
-find docs/api/ -type f -name '*.html' -exec sed -i -e 's/Sponsored and developed/Documentation generated/g' {} \;
-# Remove the copyright declaration
-find docs/api/ -type f -name '*.html' -exec sed -i -e 's/© [0-9]* Copyright//' {} \;
+./gradlew dokkaHtmlMultiModule
 
 # Copy over any static + API docs to our $DOCS_ROOT
 cp -R docs/* $DOCS_ROOT
