389a8d8395229b770b524bfbca52d170d42bd99c
Alex Vanyo <vanyo@google.com>
2021-08-11 14:08:42 -0700

    Finish checksum.sh sentence

--- checksum.sh
+++ checksum.sh
@@ -19,5 +19,5 @@ done < <(find . -type f \( -name "build.gradle*" -o -name "dependencies.kt" -o -
 for FILE in ${FILES[@]}; do
 	echo $(checksum_file $FILE) >> $RESULT_FILE
 done
-# Now sort the file so that it is 
+# Now sort the file so that it is idempotent
 sort $RESULT_FILE -o $RESULT_FILE
